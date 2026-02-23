bf9212b28a5afea58fcab5bb56a31290328d71a7
Oswaldo Ruiz <oswaldo.ruiz@seneca-corp.com>
2026-02-23 10:59:56 -0500

    API eliminación de varios empleados

--- app/api/v1/routes/empleado.py
+++ app/api/v1/routes/empleado.py
@@ -101,7 +101,6 @@ async def actualizar_empleado(
         raise HTTPException(status_code=500, detail={"error": "Error interno del servidor", "detail": str(e)})
 
 
-
 @router.delete("", response_model=SuccessResponse)
 async def eliminar_empleados(
     service: EmpleadoDependencia,
